c626dc8065613a15cce4753edd1561e46fd3729c
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-01 16:36:22 +0200

    add a bunch of comments

--- src/engine/dirkengine.cpp
+++ src/engine/dirkengine.cpp
@@ -72,6 +72,7 @@ void DirkEngine::initVulkan() {
 }
 
 void DirkEngine::createVulkanInstance() {
+    // vk app
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "DirkEngine";
@@ -80,6 +81,7 @@ void DirkEngine::createVulkanInstance() {
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_4;
 
+    // vk instance
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
@@ -122,13 +124,14 @@ void DirkEngine::createSurface() {
 }
 
 void DirkEngine::getPhysicalDevice() {
+    // get available devices
     uint32_t deviceCount = 0;
     vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
     assert(deviceCount > 0);
-
     std::vector<VkPhysicalDevice> devices(deviceCount);
     vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
+    // rank each available device
     std::multimap<int, VkPhysicalDevice> candidates;
 
     for (const auto& device : devices) {
@@ -185,9 +188,9 @@ int DirkEngine::getDeviceSuitability(VkPhysicalDevice device) {
 QueueFamilyIndices DirkEngine::findQueueFamilies(VkPhysicalDevice device) {
     QueueFamilyIndices indices;
 
+    // available queue families
     uint32_t queueFamilyCount = 0;
     vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
-
     std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
     vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
 
@@ -258,9 +261,9 @@ SwapChainSupportDetails DirkEngine::querySwapChainSupport(VkPhysicalDevice devic
 void DirkEngine::createLogicalDevice() {
     QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
 
+    // queues
     std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
     std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
-
     float queuePriority = 1.f;
     for (uint32_t queueFamily : uniqueQueueFamilies) {
         VkDeviceQueueCreateInfo queueCreateInfo{};
