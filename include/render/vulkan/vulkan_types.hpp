06b81c858b0e2bf81240ee658099825f1cde877e
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-08 15:16:15 +0200

    create vertex type for rendering

---
+++ include/render/vulkan/vulkan_types.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "render/render_types.hpp"
+#include "vulkan/vulkan_enums.hpp"
+#include "vulkan/vulkan_structs.hpp"
+
+struct VulkanVertex : Vertex {
+    static vk::VertexInputBindingDescription getBindingDescription() {
+        return { 0, sizeof(Vertex), vk::VertexInputRate::eVertex };
+    }
+
+    static std::array<vk::VertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        return {
+            vk::VertexInputAttributeDescription(0, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, pos)),
+            vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color))
+        };
+    }
+};
