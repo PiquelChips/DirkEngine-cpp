aff5d489f04a92a6a6702a03ddee7e4662b9ebce
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-08 22:31:05 +0200

    fix rectangle not being displayed

--- src/render/vulkan/vulkan.cpp
+++ src/render/vulkan/vulkan.cpp
@@ -1107,7 +1107,7 @@ void VulkanRenderer::drawFrame() {
 void VulkanRenderer::updateMVP(float deltaTime) {
     ModelViewProjection mvp{
         .model = glm::rotate(glm::mat4(1.f), deltaTime * glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f)),
-        .view = glm::lookAt(glm::vec3(2.f, 2.f, 2.f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 0.f, 0.f)),
+        .view = glm::lookAt(glm::vec3(2.f, 2.f, 2.f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 0.f, 1.f)),
         .proj = glm::perspective(glm::radians(45.f), static_cast<float>(swapChainExtent.width) / static_cast<float>(swapChainExtent.height), .1f, 10.f),
     };
 
