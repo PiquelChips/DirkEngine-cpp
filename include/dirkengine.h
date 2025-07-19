d57470ec3d58740335dac82883afe31dc2dab278
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-19 19:17:50 +0200

    add dirkengine.h with all needed includes

---
+++ include/dirkengine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "core/globals.hpp"
+#include "engine/actor.hpp"
+#include "engine/component.hpp"
+#include "engine/dirkengine.hpp"
+#include "engine/game.hpp"
+#include "render/render_types.hpp"
+#include "render/renderer.hpp"
+#include "render/renderer_types.hpp"
+#include "resources/resource_manager.hpp"
