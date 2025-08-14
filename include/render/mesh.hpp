3e218b3fadb792f9bb99b21e1a793133b0ed9dcb
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-08-14 18:43:59 +0200

    update mesh with vulkan stuff

--- include/render/mesh.hpp
+++ include/render/mesh.hpp
@@ -1,6 +1,8 @@
 #include "core/globals.hpp"
-
 #include "engine/actor.hpp"
+
+#include "vulkan/vulkan_handles.hpp"
+#include "vulkan_types.hpp"
 
 namespace dirk {
 
@@ -24,6 +26,33 @@ private:
     Actor* owningActor;
 
     bool isVisible = false;
+
+    Vulkan(Actor* owningActor);
+
+    vk::DescriptorSetLayout createDescriptorSetLayout();
+    vk::DescriptorPool createDescriptorPool();
+    ImageMemoryView createTextureResources();
+    vk::Sampler createTextureSampler();
+
+    // vertices & indices
+    vk::Buffer createVertexBuffer();
+    vk::Buffer createIndexBuffer();
+
+    vk::DescriptorSetLayout descriptorSetLayout;
+    vk::DescriptorPool descriptorPool;
+
+    std::shared_ptr<const Model> model;
+
+    vk::Buffer vertexBuffer;
+    vk::DeviceMemory vertexBufferMemory;
+
+    vk::Buffer indexBuffer;
+    vk::DeviceMemory indexBufferMemory;
+
+    ImageMemoryView textureImageMemoryView;
+
+    uint32_t mipLevels;
+    vk::Sampler textureSampler;
 };
 
 } // namespace dirk
