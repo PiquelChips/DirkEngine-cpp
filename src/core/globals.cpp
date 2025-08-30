7a8b6e9569e1655b81d83449cd99501c2d984d8b
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-08-30 18:33:04 +0200

    move tinygltf impl to globals.cpp

---
+++ src/core/globals.cpp
@@ -0,0 +1,7 @@
+#include "core/globals.hpp"
+
+// TINY GLTF
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "tinygltf/tiny_gltf.h"
