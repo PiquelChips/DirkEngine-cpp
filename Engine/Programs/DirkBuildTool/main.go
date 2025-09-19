b77ed3b9d774e493d1fe205f11f3c41f06104eb5
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-09-19 18:27:43 +0200

    add cli to main

--- Engine/Programs/DirkBuildTool/main.go
+++ Engine/Programs/DirkBuildTool/main.go
@@ -10,22 +10,49 @@ import (
 	"DirkBuildTool/setup"
 )
 
+func usage() {
+	fmt.Printf("usage: DirkBuildTool [target] [build type]\n")
+}
+
 func main() {
 	if err := output.GetOutDirs(); err != nil {
 		panic(err)
 	}
 
-	if len(os.Args) != 1 {
-		fmt.Printf("cli not implemented yet, please run without params\n")
+	var buildConfig *setup.BuildConfig
+	if len(os.Args) == 1 {
+		buildConfig = &setup.BuildConfig{
+			Target:    "Editor",
+			BuildType: "Development",
+			Optimize:  false,
+			Shipping:  false,
+		}
+	} else if len(os.Args) == 3 {
+		buildConfig = &setup.BuildConfig{
+			Target:    os.Args[1],
+			BuildType: os.Args[2],
+		}
+
+		switch buildConfig.BuildType {
+		case "Development":
+			buildConfig.Optimize = false
+			buildConfig.Shipping = false
+		case "Shipping":
+			buildConfig.Optimize = true
+			buildConfig.Shipping = true
+		default:
+			log.Printf("Cannot build target %s for %s. Defaulting to Development.\n", buildConfig.Target, buildConfig.BuildType)
+			buildConfig.BuildType = "Development"
+			buildConfig.Optimize = false
+			buildConfig.Shipping = false
+		}
+	} else {
+		fmt.Printf("Invalid number of arguments.\n")
+		usage()
+		os.Exit(1)
 		return
 	}
 
-	buildConfig := &setup.BuildConfig{
-		Target:    "Editor",
-		BuildType: "Development",
-		Optimize:  false,
-		Shipping:  false,
-	}
 	if err := setup.Setup(buildConfig); err != nil {
 		panic(err)
 	}
