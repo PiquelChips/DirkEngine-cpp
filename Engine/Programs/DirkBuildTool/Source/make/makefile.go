be35f453fec3b986c92653b1fd70e64b64e57ec6
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-09-21 14:01:55 +0200

    clean makefiles

--- Engine/Programs/DirkBuildTool/Source/make/makefile.go
+++ Engine/Programs/DirkBuildTool/Source/make/makefile.go
@@ -46,87 +46,85 @@ type CppMakefile struct {
 	LdFlags, CFlags    string
 	IsLib, IsStatic    bool
 	Optimize           bool
-	buffer             *bytes.Buffer
 }
 
 func (m *CppMakefile) ToBytes() ([]byte, error) {
-	m.buffer = bytes.NewBuffer(nil)
+	buffer := bytes.NewBuffer(nil)
 
-	writeVar(m.buffer, "NAME", m.Name)
+	writeVar(buffer, "NAME", m.Name)
 
-	m.buffer.WriteString("TARGET=")
+	buffer.WriteString("TARGET=")
 	if m.IsLib {
-		m.buffer.WriteString("lib")
+		buffer.WriteString("lib")
 	}
-	m.buffer.WriteString(m.Target)
+	buffer.WriteString(m.Target)
 	if m.IsLib {
 		if m.IsStatic {
-			m.buffer.WriteString(".a")
+			buffer.WriteString(".a")
 		} else {
-			m.buffer.WriteString(".so")
+			buffer.WriteString(".so")
 		}
 	}
-	newLine(m.buffer)
+	newLine(buffer)
 
-	writeVar(m.buffer, "ROOT_DIR", m.RootDir)
-	writeVar(m.buffer, "BUILD_TYPE", m.BuildType)
-	writeVar(m.buffer, "CFLAGS", m.CFlags)
+	writeVar(buffer, "ROOT_DIR", m.RootDir)
+	writeVar(buffer, "BUILD_TYPE", m.BuildType)
+	writeVar(buffer, "CFLAGS", m.CFlags)
 
 	if m.Optimize {
-		m.buffer.WriteString("CFLAGS+= -O3\n")
+		buffer.WriteString("CFLAGS+= -O3\n")
 	}
 
 	cxxFlags := []string{}
 	for _, dir := range m.IncDirs {
 		cxxFlags = append(cxxFlags, fmt.Sprintf("-I%s", dir))
 	}
-	writeVar(m.buffer, "CXXFLAGS", cxxFlags...)
+	writeVar(buffer, "CXXFLAGS", cxxFlags...)
 
 	defines := []string{}
 	for _, define := range m.Defines {
 		defines = append(defines, fmt.Sprintf("-D%s", define))
 	}
-	writeVar(m.buffer, "DEFINES", defines...)
+	writeVar(buffer, "DEFINES", defines...)
 
-	writeVar(m.buffer, "LDFLAGS", m.LdFlags)
+	writeVar(buffer, "LDFLAGS", m.LdFlags)
 
 	ldLibs := []string{}
 	for _, lib := range m.Libs {
 		ldLibs = append(ldLibs, fmt.Sprintf("-l%s", lib))
 	}
-	writeVar(m.buffer, "LDLIBS", ldLibs...)
+	writeVar(buffer, "LDLIBS", ldLibs...)
 
-	writeBase(m.buffer, "base")
-	writeBase(m.buffer, "compilation")
+	writeBase(buffer, "base")
+	writeBase(buffer, "compilation")
 
 	if !m.IsLib && !m.IsStatic {
 		// exec
-		writeBase(m.buffer, "cxx_lnk")
+		writeBase(buffer, "cxx_lnk")
 	} else if !m.IsLib && m.IsStatic {
 		// static exec
-		writeBase(m.buffer, "cxx_lnk")
+		writeBase(buffer, "cxx_lnk")
 	} else if m.IsLib && !m.IsStatic {
 		// shared lib
-		m.buffer.WriteString("LDFLAGS+= -shared\n")
-		writeBase(m.buffer, "cxx_lnk")
+		buffer.WriteString("LDFLAGS+= -shared\n")
+		writeBase(buffer, "cxx_lnk")
 	} else if m.IsLib && m.IsStatic {
 		// static lib
-		writeBase(m.buffer, "ar_lnk")
+		writeBase(buffer, "ar_lnk")
 	}
 
-	return m.buffer.Bytes(), nil
+	return buffer.Bytes(), nil
 }
 
 type ShaderMakefile struct {
 	Path    string
 	RootDir string
-	buffer  *bytes.Buffer
 }
 
 func (m *ShaderMakefile) ToBytes() ([]byte, error) {
-	m.buffer = bytes.NewBuffer(nil)
-	writeVar(m.buffer, "ROOT_DIR", m.RootDir)
-	writeVar(m.buffer, "SHADER_DIR", m.Path)
-	writeBase(m.buffer, "shaders")
-	return m.buffer.Bytes(), nil
+	buffer := bytes.NewBuffer(nil)
+	writeVar(buffer, "ROOT_DIR", m.RootDir)
+	writeVar(buffer, "SHADER_DIR", m.Path)
+	writeBase(buffer, "shaders")
+	return buffer.Bytes(), nil
 }
