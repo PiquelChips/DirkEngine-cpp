a1d524318ae7a736552ea3687d156a69f6e2ca18
Piquel <piquel@piquel.fr>
2025-11-25 23:07:01 +0100

    Add TODO for debug build flags in makefile.go
    Added TODO comment to conditionally set flags for debug builds.

--- Engine/Programs/DirkBuildTool/Source/make/makefile.go
+++ Engine/Programs/DirkBuildTool/Source/make/makefile.go
@@ -100,6 +100,7 @@ func (m *CppMakefile) ToBytes() ([]byte, error) {
 	}
 	writeVar(buffer, "LDLIBS", ldLibs...)
 
+	// TODO: only set these flags in debug build
 	buffer.WriteString("LDFLAGS+= -g\n")
 	buffer.WriteString("CXXFLAGS+= -g\n")
 
