7f0fe0f266c17e362936c981ed44ff2ff637f816
PiquelChips <piquel@piquel.fr>
2025-12-26 18:08:38 +0100

    replace protocol generation script with a makefile

--- Engine/Thirdparty/wayland-protocols/generate-protocols.sh
+++
@@ -1,3 +0,0 @@
-wayland-scanner client-header < protocols/wayland.xml > include/wayland-client-protocol.h
-wayland-scanner client-header < protocols/xdg-shell.xml > include/xdg-shell-client-protocol.h
-wayland-scanner public-code < protocols/xdg-shell.xml > src/xdg-shell-protocol.cpp
