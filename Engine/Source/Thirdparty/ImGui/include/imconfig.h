f02b1414cbb517467a004ef62a10366c8bd33e02
PiquelChips <piquel@piquel.fr>
2026-01-04 11:09:46 +0100

    add ImVec conversions to glm types

--- Engine/Source/Thirdparty/ImGui/include/imconfig.h
+++ Engine/Source/Thirdparty/ImGui/include/imconfig.h
@@ -103,15 +103,17 @@
 //---- Define constructor and implicit cast operators to convert back<>forth between your math types and ImVec2/ImVec4.
 // This will be inlined as part of ImVec2 and ImVec4 class declarations.
 #include "vulkan/vulkan.hpp"
+#include "glm/glm.hpp"
 #define IM_VEC2_CLASS_EXTRA                                                     \
         constexpr ImVec2(const vk::Extent2D& f) : x(static_cast<float>(f.width)), y(static_cast<float>(f.height)) {}                   \
-        operator vk::Extent2D() const { return vk::Extent2D(static_cast<std::uint32_t>(x),static_cast<std::uint32_t>(y)); }
+        operator vk::Extent2D() const { return vk::Extent2D(static_cast<std::uint32_t>(x),static_cast<std::uint32_t>(y)); } \
+        constexpr ImVec2(const glm::vec2& f) : x(f.x), y(f.y) {}                   \
+        operator glm::vec2() const { return glm::vec2(x, y); } 
 
-/*
 #define IM_VEC4_CLASS_EXTRA                                                     \
-        constexpr ImVec4(const MyVec4& f) : x(f.x), y(f.y), z(f.z), w(f.w) {}   \
-        operator MyVec4() const { return MyVec4(x,y,z,w); }
-*/
+        constexpr ImVec4(const glm::vec4& f) : x(f.x), y(f.y), z(f.z), w(f.w) {}                   \
+        operator glm::vec4() const { return glm::vec4(x, y, z, w); } 
+
 //---- ...Or use Dear ImGui's own very basic math operators.
 #define IMGUI_DEFINE_MATH_OPERATORS
 
