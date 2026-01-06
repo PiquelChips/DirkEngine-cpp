82c0fe72bedbd86f82b136643a4cb7027dcac6bb
PiquelChips <piquel@piquel.fr>
2026-01-06 16:26:09 +0100

    add name member to event

--- Engine/Source/Core/include/Events/Event.hpp
+++ Engine/Source/Core/include/Events/Event.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
+#include <string_view>
 #include <typeindex>
 
 namespace dirk {
 
-#define DEFINE_EVENT_TYPE(type)                                     \
-public:                                                             \
-    static std::type_index getStaticType() { return typeid(type); } \
-    std::type_index getType() const override { return getStaticType(); }
+#define DEFINE_EVENT_TYPE(type)                                          \
+public:                                                                  \
+    static std::type_index getStaticType() { return typeid(type); }      \
+    std::type_index getType() const override { return getStaticType(); } \
+    std::string_view name() const override { return #type; }
 
 class Event {
 public:
     virtual ~Event() = default;
     virtual std::type_index getType() const = 0;
+    virtual std::string_view name() const = 0;
 };
 
 } // namespace dirk
