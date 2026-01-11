56582406376a0e61fbccb5543ac3eeb678fef1f9
PiquelChips <piquel@piquel.fr>
2026-01-11 19:23:35 +0100

    setup detection

--- Engine/Programs/DirkBuildTool/Source/setup/setup.go
+++ Engine/Programs/DirkBuildTool/Source/setup/setup.go
@@ -1,7 +1,27 @@
 package setup
 
-import "log"
+import (
+	"log"
+	"os/exec"
+)
+
+func check(bin string) error {
+	_, err := exec.LookPath(bin)
+	return err
+}
 
 func Setup() error {
 	log.Printf("Running setup...")
+
+	if err := check("g++"); err != nil {
+		return err
+	}
+	log.Printf("Found the C++ compiler.")
+
+	if err := check("make"); err != nil {
+		return err
+	}
+	log.Printf("Found GNU Make.")
+
+	return nil
 }
